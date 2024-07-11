383b2ba3dc6658294d0ff2fbd77d4628aebbf34b
Varshini Teneti <88938745+varshiniteneti@users.noreply.github.com>
2024-07-10 20:28:42 -0400

    Add files via upload

---
+++ Number_game_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_game_challengeApp.swift
+//  Number_game_challenge
+//
+//  Created by Varshini Teneti on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Number_game_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
